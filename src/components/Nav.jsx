faadd081970aedfff040feaaa5e6466d34c23d4c
CreateYourReality <timo.moewes@icloud.com>
2023-06-15 16:49:28 +0200

    final

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import {Link} from "react-router-dom"
+import bierImg from "../assets/beer.png"
 
 const Nav = () => {
     return ( 
         <nav>
-            <Link to="/"><img src="../../public/beer.png" alt="" /></Link>
+            <Link to="/"><img src={bierImg} alt="" /></Link>
         </nav>
      );
 }
